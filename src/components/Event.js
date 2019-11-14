8613e583ecbc228c7a68c5786bec3aa660d4dafd
Daniel Zarate <daniel@zarate.org>
2019-11-14 12:09:13 -0600

    added type to input

--- src/components/Event.js
+++ src/components/Event.js
@@ -2,16 +2,16 @@ import React from 'react';
 import '../App.css';
 
 class Event extends React.Component {
-  render () {
+  render() {
     return (
       <div className="event">
         <div className="event\_inner">
           <h1 className="header">Add Event</h1>
           <div className="input">
-              <input />
+            <input type="text" />
           </div>
           <div className="submit">
-          <button onClick={this.props.closeEvent}>Submit</button>
+            <button onClick={this.props.closeEvent}>Submit</button>
           </div>
         </div>
       </div>
